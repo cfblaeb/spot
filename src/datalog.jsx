c3c8ff291bb45e2b0cb5618b6b1eca92440ac15f
Lasse Ebdrup Pedersen <laeb@dtu.dk>
2026-02-22 14:32:16 +0100

    issue with datalog showing time as 1 hour later than actual.

--- src/datalog.jsx
+++ src/datalog.jsx
@@ -58,7 +58,11 @@ export default class DTGraph extends Component {
 			const {label} = this.props
 			fetch("/historic_json/" + label + "/" + start_time + "/" + end_time)
 				.then(r => r.json())
-				.then(data => this.setState({visible_data: JSON.parse(data)}))
+				.then(data => {
+					// Convert x (date string) to Date object in UTC
+					let fixedData = JSON.parse(data).map(d => ({...d, x: new Date(d.x + 'Z')}))
+					this.setState({visible_data: fixedData})
+				})
 				.catch(e => console.log(e))
 		}
 	}
